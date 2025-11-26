7c5df0428d5d4bac504cf1941388d461ba2872d5
XavierEgan <xavieregan2@gmail.com>
2025-11-26 15:08:34 +1100

    remove overloads

--- src/ObjParser/Mesh.cpp
+++ src/ObjParser/Mesh.cpp
@@ -1,21 +1,3 @@
 #include "Mesh.hpp"
 
 pt::Mesh::Mesh(std::string name) : name(name) {}
-
-bool pt::Mesh::operator==(const Mesh& other) const {
-	return vertices == other.vertices &&
-		vertexTextureCoordinates == other.vertexTextureCoordinates &&
-		vertexNormals == other.vertexNormals &&
-		vertexIndexes == other.vertexIndexes &&
-		vertexTextureCoordinatesIndexes == other.vertexTextureCoordinatesIndexes &&
-		vertexNormalsIndexes == other.vertexNormalsIndexes;
-}
-
-bool pt::Mesh::operator!=(const Mesh& other) const {
-	return vertices != other.vertices &&
-		vertexTextureCoordinates != other.vertexTextureCoordinates &&
-		vertexNormals != other.vertexNormals &&
-		vertexIndexes != other.vertexIndexes &&
-		vertexTextureCoordinatesIndexes != other.vertexTextureCoordinatesIndexes &&
-		vertexNormalsIndexes != other.vertexNormalsIndexes;
-}
