fc7f1c517837a02f0f4d53a022db4f33b8abf164
XavierEgan <xavieregan2@gmail.com>
2026-01-01 18:21:42 +1100

    remove python and change to hpp

---
+++ obj_parser/obj_parser.hpp
@@ -0,0 +1,16 @@
+#include "include/ext/glm/glm.hpp"
+#include "include/Mesh.hpp"
+#include "include/Material.hpp"
+#include "include/ObjParserError.hpp"
+#include "include/MtlParser.hpp"
+#include "include/ObjParser.hpp"
+
+#ifdef OBJ_PARSER_IMPLEMENTATION
+
+#include "src/ObjParser/Mesh.cpp"
+#include "src/ObjParser/Material.cpp"
+#include "src/ObjParser/ObjParserError.cpp"
+#include "src/ObjParser/MtlParser.cpp"
+#include "src/ObjParser/ObjParser.cpp"
+
+#endif
